f5d5be8982f504da44210ec08d3161ff71ae6e80
Umesh Chandra <umesh@pranathiss.local>
2025-04-17 03:10:25 -0400

    Initial commit - added mobile-style calculator with styled CSS

---
+++ script.js
@@ -0,0 +1,17 @@
+function press(value){
+    document.getElementById("display").value += value;
+}
+
+function calculate(){
+    const display = document.getElementById("display");
+    try{
+        const result = eval(display.value)
+        display.value = result;
+    } catch{
+        display.value = "Error";
+    }
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
