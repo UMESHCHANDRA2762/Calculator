ec2bb7b6dd78973c2fc5dd7ea1c0c101d5514728
Umesh Chandra <umesh@pranathiss.local>
2025-04-17 03:27:54 -0400

    added backspace

--- script.js
+++ script.js
@@ -15,3 +15,8 @@ function calculate(){
 function clearDisplay() {
     document.getElementById("display").value = "";
   }
+
+function backspace(){
+   const display =document.getElementById("display");
+   display.value = display.value.slice(0,-1);
+}
